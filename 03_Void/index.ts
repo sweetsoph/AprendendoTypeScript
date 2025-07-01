5d40345ef8a39f903ca2ef1ff0dd315357b125be
Sophia Verardo <sophia.verardods@gmail.com>
2025-07-01 18:54:10 -0300

    void functions

---
+++ 03_Void/index.ts
@@ -0,0 +1,7 @@
+let pedido = (): void => {
+    console.log("Pedido realizado com sucesso!");
+}
+
+let pedido2 = (): void => {
+    return "Pedido realizado com sucesso!" // não deixa pois a função foi inicializada como void
+}
