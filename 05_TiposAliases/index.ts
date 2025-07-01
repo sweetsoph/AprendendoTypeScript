8190522b6b057c85682016ee6570762645e80191
Sophia Verardo <sophia.verardods@gmail.com>
2025-07-01 19:03:05 -0300

    criando meu próprio tipo de dado

---
+++ 05_TiposAliases/index.ts
@@ -0,0 +1,5 @@
+// é possível apelidar um determinado tipo de dado para facilitar a leitura e repetição dele no código
+type alphanumeric = string | number;
+
+const id: alphanumeric = "12345"; // string
+const id2: alphanumeric = 12345; // number
